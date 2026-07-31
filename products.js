5943087ee7de689e9b050663e98efc54d507e02a
mugdhomugdho77-collab <mugdhomugdho77@gmail.com>
2026-08-01 02:24:07 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,47 @@
+// এই ফাইলে ক্লাইন্টের সব প্রোডাক্টের ডেটা থাকবে। 
+// নতুন ক্লাইন্ট আসলে শুধু এই ডেটাগুলো বদলালেই পুরো সাইট আপডেট হয়ে যাবে!
+
+const products = [
+    {
+        id: 1,
+        name: "SUNFEST Off White Pinstripe Shirt",
+        category: "Shirts",
+        price: 1250,
+        image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=500"
+    },
+    {
+        id: 2,
+        name: "SUNFEST Slate Blue Stripe Shirt",
+        category: "Shirts",
+        price: 1250,
+        image: "https://images.unsplash.com/photo-1596755094514-f87e34085b2c?w=500"
+    },
+    {
+        id: 3,
+        name: "SKYFALL Grey Pinstripe Shirt",
+        category: "Shirts",
+        price: 1250,
+        image: "https://images.unsplash.com/photo-1589810645855-6e3e55171dfb?w=500"
+    },
+    {
+        id: 4,
+        name: "SKYFALL Jet Black Casual Shirt",
+        category: "Shirts",
+        price: 1250,
+        image: "https://images.unsplash.com/photo-1618354691373-d851সি5c3a90?w=500"
+    },
+    {
+        id: 5,
+        name: "Classic Formal Brown Trouser",
+        category: "Pants",
+        price: 1650,
+        image: "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?w=500"
+    },
+    {
+        id: 6,
+        name: "Slim Fit Stylish Denim Pant",
+        category: "Pants",
+        price: 1850,
+        image: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?w=500"
+    }
+];
